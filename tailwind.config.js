b7e2694617fa7a45402dd1ee6ea029134cb6c5e1
Jakub <havier1993@gmail.com>
2023-12-14 17:18:58 +0100

    creating navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Roboto': ['Roboto', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
